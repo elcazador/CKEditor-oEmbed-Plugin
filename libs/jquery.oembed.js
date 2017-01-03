d88860154de33425f9b0d9a5c13902c38860b904
Mathew Winstone <mathew.winstone@gmail.com>
2017-01-03 17:01:36 -0500

    fix(units)
    Add individual units

--- libs/jquery.oembed.js
+++ libs/jquery.oembed.js
@@ -676,7 +676,7 @@
     $.fn.oembed.providers = [
         //Video
         new $.fn.oembed.OEmbedProvider("youtube", "video", ["youtube\\.com/watch.+v=[\\w-]+&?", "youtu\\.be/[\\w-]+", "youtube.com/embed"], checkProtocol() + 'www.youtube.com/embed/$1?wmode=transparent', {
-        templateRegex: /.*(?:v\=|be\/|embed\/)([\w\-]+)(?:&[^t]=[\w]*)*(?:&t=([\d]+[hms](?:\d+[ms]){1,2}|[\d]+)?)?/,embedtag: {tag: 'iframe',width: '425',height: '349'}}),
+        templateRegex: /.*(?:v\=|be\/|embed\/)([\w\-]+)(?:&[^t]=[\w]*)*(?:&t=([\d]+[hms](?:\d+[ms]){1,2}|[\d]+[h]|[\d]+[m]|[\d]+[s])?)?/,embedtag: {tag: 'iframe',width: '425',height: '349'}}),
         /*new $.fn.oembed.OEmbedProvider("youtube", "video", ["youtube\\.com/watch.+v=[\\w-]+&?", "youtu\\.be/[\\w-]+"], checkProtocol() + 'www.youtube.com/oembed', { useYQL: 'json' }),*/
         new $.fn.oembed.OEmbedProvider("youtubeiframe", "video", ["youtube.com/embed"], "$1?wmode=transparent",
             { templateRegex: /(.*)/, embedtag: { tag: 'iframe', width: '425', height: '349' } }),
